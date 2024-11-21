6c22b6e60cd9ff6f9d6cf0e393636c1e67cdfaf6
Jonas Raoni Soares da Silva <github@raoni.org>
2024-11-22 02:28:02 +0300

    Feature main 7512 use accept language (#10611)
    * pkp/pkp-lib#7512 Added Accept-Language header into the locale detection
    
    * pkp/pkp-lib#7512 Fixed call to PKPRequest::getPreferredLanguage()
    
    * pkp/pkp-lib#7512 Give preference to the server preferred locale if there's no match with the Accept-Language header
    
    * pkp/pkp-lib#7512 Updated code to use the Laravel request directly

--- classes/i18n/Locale.php
+++ classes/i18n/Locale.php
@@ -563,4 +563,19 @@ class Locale implements LocaleInterface
         }
         return app(\Illuminate\Http\Request::class)->getPreferredLanguage([$serverPreference, ...$supportedLocales]);
     }
+
+    /**
+     * Retrieve the preferred user locale from our supported locales using the Accept-Language header
+     * If there's no match, it falls back to the server's primary locale
+     */
+    private function getPreferredLocale(): ?string
+    {
+        $serverPreference = $this->getPrimaryLocale() ?: LocaleInterface::DEFAULT_LOCALE;
+        $supportedLocales = array_values($this->_getSupportedLocales());
+        // Move the server preference to the top, in case the user preference doesn't match with the supported locales, the server one will be picked
+        if (is_int($index = array_search($serverPreference, $supportedLocales))) {
+            array_splice($supportedLocales, $index, 1);
+        }
+        return app(\Illuminate\Http\Request::class)->getPreferredLanguage([$serverPreference, ...$supportedLocales]);
+    }
 }
